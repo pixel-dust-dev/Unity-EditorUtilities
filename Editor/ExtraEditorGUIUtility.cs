c89954499d3ca6e402136a22e4aa3354b58311b3
Alistair O'Brien <alistair.m.obrien@gmail.com>
2022-04-27 15:36:13 +0900

    initial commit

---
+++ Editor/ExtraEditorGUIUtility.cs
@@ -0,0 +1,9 @@
+﻿using UnityEditor;
+
+public class ExtraEditorGUIUtility
+{
+    public static float SingleLineHeight()
+    {
+        return EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
+    }
+}
